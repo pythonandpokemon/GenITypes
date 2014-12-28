22e646c14cba8525927bd79ad09fa3dab179987d
pandp <python.and.pokemon@gmail.com>
2014-12-28 15:56:53 -0500

    Unique function stuff
    made all this

---
+++ scoreTypesCalc.py
@@ -0,0 +1,94 @@
+# Import stuff
+import pandas 	# for dataframes/series
+import numpy	# for math
+import itertools# for combinations
+
+# Make the type chart
+typeChart = { 
+'ATTACK DOWN DEFENSE RIGHT' : ['NORMAL', 'FIGHTING', 'FLYING', 'POISON', 'GROUND', 'ROCK', 'BUG', 'GHOST', 'FIRE', 'WATER', 'GRASS', 'ELECTRIC', 'PSYCHIC', 'ICE', 'DRAGON'], 
+'NORMAL' : [1.0, 2.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0], 
+'FIGHTING' : [1.0, 1.0, 2.0, 1.0, 1.0, 0.5, 0.5, 1.0, 1.0, 1.0, 1.0, 1.0, 2.0, 1.0, 1.0], 
+'FLYING' : [1.0, 0.5, 1.0, 1.0, 0.0, 2.0, 0.5, 1.0, 1.0, 1.0, 0.5, 2.0, 1.0, 2.0, 1.0], 
+'POISON' : [1.0, 0.5, 1.0, 0.5, 2.0, 1.0, 2.0, 1.0, 1.0, 1.0, 0.5, 1.0, 2.0, 1.0, 1.0], 
+'GROUND' : [1.0, 1.0, 1.0, 0.5, 1.0, 0.5, 1.0, 1.0, 1.0, 2.0, 2.0, 0.0, 1.0, 2.0, 1.0], 
+'ROCK' : [0.5, 2.0, 0.5, 0.5, 2.0, 1.0, 1.0, 1.0, 0.5, 2.0, 2.0, 1.0, 1.0, 1.0, 1.0], 
+'BUG' : [1.0, 0.5, 2.0, 2.0, 0.5, 2.0, 1.0, 1.0, 2.0, 1.0, 0.5, 1.0, 1.0, 1.0, 1.0], 
+'GHOST' : [0.0, 0.0, 1.0, 0.5, 1.0, 1.0, 0.5, 2.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0], 
+'FIRE' : [1.0, 1.0, 1.0, 1.0, 2.0, 2.0, 0.5, 1.0, 0.5, 2.0, 0.5, 1.0, 1.0, 1.0, 1.0], 
+'WATER' : [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.5, 0.5, 2.0, 2.0, 1.0, 0.5, 1.0], 
+'GRASS' : [1.0, 1.0, 2.0, 2.0, 0.5, 1.0, 2.0, 1.0, 2.0, 0.5, 0.5, 0.5, 1.0, 2.0, 1.0], 
+'ELECTRIC' : [1.0, 1.0, 0.5, 1.0, 2.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.5, 1.0, 1.0, 1.0], 
+'PSYCHIC' : [1.0, 0.5, 1.0, 1.0, 1.0, 1.0, 2.0, 0.0, 1.0, 1.0, 1.0, 1.0, 0.5, 1.0, 1.0], 
+'ICE' : [1.0, 2.0, 1.0, 1.0, 1.0, 2.0, 1.0, 1.0, 2.0, 1.0, 1.0, 1.0, 1.0, 0.5, 1.0], 
+'DRAGON' : [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.5, 0.5, 0.5, 0.5, 1.0, 2.0, 2.0] }
+typeChart = pandas.DataFrame(typeChart)
+
+for i in xrange(1,len(typeChart)+1):
+	for j in xrange(i+1,len(typeChart)+1):
+		print(typeChart.columns.values[i] + typeChart.columns.values[j])
+		typeChart[typeChart.columns.values[i] + "/" + typeChart.columns.values[j]] = pandas.Series(typeChart[typeChart.columns.values[i]]*typeChart[typeChart.columns.values[j]],index=typeChart.index)
+
+# Getter for the type chart
+def getTypeChart():
+	return(typeChart)
+
+# Build empty dataframe for all the combinations of Types
+# Number of different Types
+nTypes = len(typeChart)
+# Number of different Combos
+#    Number of combinations with two different types
+nCombos = (nTypes * (nTypes-1))/2
+#    Plus number of single types
+nCombos = nCombos + nTypes
+# Build the dataframe
+typeMatches = pandas.DataFrame(index=numpy.arange(nCombos),columns=['Type1','Type2'])
+
+# Get a list of all the types
+types=typeChart.ix[:,0]
+# Don't forget a blank for single type Pokemon
+types[15]='NA'
+
+# Fill in the DataFrame with all the combinations
+count = 0
+for i in itertools.combinations(types,2):
+	typeMatches.ix[count] = i
+	count = count+1
+
+# Getter for the type matches	
+def getTypesList():
+	return(typeMatches)
+
+# Get the stats for Defense 	
+def scoreTypesDefense(function):
+	# Use this function to apply through the type matches
+	# This uses the input function for the computation
+	def defFunc(types):
+		type1Stats = typeChart[types[0]]
+		type2Stats = None
+		if types[1] != 'NA':
+			type2Stats = typeChart[types[1]]
+		return(function(type1Stats,type2Stats))
+	# Apply through and name the DataFrame
+	stats = pandas.DataFrame(typeMatches.apply(defFunc,axis=1))
+	stats.columns = [function.__name__]
+	return stats
+
+# Get the stats for Attack	
+def scoreTypesAttack(function):
+	# Use this function to apply through the type matches
+	# This uses the input function for the computation
+	def attFunc(types):
+		type1Stats = typeChart[typeChart.ix[:,0]==types[0]].ix[:,1:].squeeze()
+		type1Stats.name = types[0]
+		type2Stats = None
+		if types[1] != 'NA':
+			type2Stats = typeChart[typeChart.ix[:,0]==types[1]].ix[:,1:].squeeze()
+			type2Stats.name = types[1]
+		return(function(type1Stats,type2Stats))
+	# Apply through and name the DataFrame
+	stats = pandas.DataFrame(typeMatches.apply(attFunc,axis=1))
+	stats.columns = [function.__name__]
+	return stats	
+
+	
+	
---
+++ scoreTypesFunc.py
@@ -0,0 +1,90 @@
+import scoreTypesCalc
+# Need to stretch out the type chart to cover dual types
+
+import numpy
+import pandas
+
+typeMatches = scoreTypesCalc.getTypesList()
+#print(typeMatches)
+
+def avgDef(type1Stats,type2Stats):
+	stats = type1Stats
+	if type2Stats is not None:
+		stats = stats*type2Stats
+	return(numpy.mean(stats))
+	
+statsList = scoreTypesCalc.scoreTypesDefense(avgDef)
+#print(statsList)
+
+finalStats=pandas.merge(typeMatches,statsList,left_index=True,right_index=True)
+
+def avgAtt(type1Stats,type2Stats):
+	stats = type1Stats
+	if type2Stats is not None:
+		stats = numpy.maximum(type1Stats,type2Stats)
+	return(numpy.mean(stats))
+
+statsList = scoreTypesCalc.scoreTypesAttack(avgAtt)
+#print(statsList)
+
+finalStats=pandas.merge(finalStats,statsList,left_index=True,right_index=True)
+
+def uniDef(type1Stats,type2Stats):
+
+	def getNum(stat):
+		return {
+			0 : 100,
+			.25 : 75,
+			.5 : 50,
+			1 : 0,
+			2 : -75,
+			4 : -150,
+			}.get(stat, 0)
+
+	stats = type1Stats
+	
+	if stats.name == "BUG":
+		return None
+	else:
+		if type2Stats is not None:
+			if type2Stats.name == "BUG":
+				return None
+			stats = stats*type2Stats
+		stats = stats.apply(getNum)
+		return(numpy.mean(stats))
+
+statsList = scoreTypesCalc.scoreTypesDefense(uniDef)
+
+finalStats=pandas.merge(finalStats,statsList,left_index=True,right_index=True)
+
+def uniAtt(type1Stats,type2Stats):
+
+	def getNum(stat):
+		return {
+			0 : -100,
+			.25 : -75,
+			.5 : -50,
+			1 : 0,
+			2 : 75,
+			4 : 150,
+			}.get(stat, 0)
+
+	stats = type1Stats
+	
+	if stats.name == "BUG":
+		return None
+	else:
+		if type2Stats is not None:
+			if type2Stats.name == "BUG":
+				return None
+			stats = numpy.maximum(stats,type2Stats)
+		stats = stats.apply(getNum)
+		return(numpy.mean(stats))
+
+statsList = scoreTypesCalc.scoreTypesAttack(uniAtt)
+
+finalStats=pandas.merge(finalStats,statsList,left_index=True,right_index=True)
+
+finalStats.to_csv("C:\PandP\GenITypes\UniqueFunctionStats.csv",index=False)
+
+print('Done')
